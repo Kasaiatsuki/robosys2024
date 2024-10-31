5cf4b896bf2828b7b179674c8c669e81205d07e8
Atsuki Kasai <kasaiatsuki@gmail.com>
2024-10-31 12:39:40 +0900

    修正

--- test.bash
+++ test.bash
@@ -1,14 +1,23 @@
-#!/bin/bash
-
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2024 Atsuki Kasai
+# SPDX-License-Identifier: BSD-3-Clause
 ng () {
 	echo ${1}行目が違うよ
 	res=1
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+++ seq 5
+++ ./plus
++ out=15
+[ "${out}" = 15 ] || ng "$LINENO"
++ '[' 15 = 15 ']'
+
+[ "${res}" = 0 ] && echo OK
++ '[' 0 = 0 ']'
++ echo OK
+OK
 exit $res
-echo $?
++ exit 0
