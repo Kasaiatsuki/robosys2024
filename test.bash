8c5105792fe4bf060014494668defdbc6b16ff7a
Atsuki Kasai <kasaiatsuki@gmail.com>
2024-10-31 13:06:19 +0900

    修正

--- test.bash
+++ test.bash
@@ -7,17 +7,16 @@ ng () {
 }
 
 res=0
-
 out=$(seq 5 | ./plus)
-++ seq 5
-++ ./plus
-+ out=15
 [ "${out}" = 15 ] || ng "$LINENO"
-+ '[' 15 = 15 ']'
+out=$(echo あ | ./plus) 
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
-+ '[' 0 = 0 ']'
-+ echo OK
-OK
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
-+ exit 0
+
